f423bdd4c38cd25758047b03856ccd5cd2434423
sun2002lsw <44559110+sun2002lsw@users.noreply.github.com>
2023-09-14 00:05:02 +0900

    implement main menu button (2/3)

--- etc/button.py
+++ etc/button.py
@@ -10,15 +10,61 @@ class Button:
 		self._screen = screen
 		self._buttonRect = pygame.Rect(x - BUTTON_SIZE, y - BUTTON_SIZE / 3, Width, Height)
 		self._text = text
+		self._defaultState = True
 
-		# 글자 만들기
+		# 기본 글자로 버튼 만들기
+		self._drawDefaultButton()
+
+	# 마우스를 버튼 위에 올렸을 때
+	def Hovering(self, mousePos):
+		if not self._isInsideButton(mousePos):
+			if not self._defaultState:
+				self._drawDefaultButton()
+			return
+
+		# 굵고 흰 글자로 버튼 바꾸기
+		font = pygame.font.SysFont("malgungothic", 30, True, False)
+		surface = font.render(self._text, True, "white")
+		self._drawButton(surface)
+		self._defaultState = False
+
+	# 마우스를 누른채 버튼 위에 올렸을 때
+	def Pressing(self, mousePos):
+		if not self._isInsideButton(mousePos):
+			if not self._defaultState:
+				self._drawDefaultButton()
+			return
+
+		# 굵고 흰 글자의 눌림 표현
+		font = pygame.font.SysFont("malgungothic", 25, True, False)
+		surface = font.render(self._text, True, "white")
+		self._drawButton(surface)
+		self._defaultState = False
+
+	# 기본 상태의 버튼을 생성
+	def _drawDefaultButton(self):
 		font = pygame.font.SysFont("malgungothic", 30, False, False)
-		surface = font.render(text, True, "black")
+		surface = font.render(self._text, True, "black")
+		self._drawButton(surface)
 
+	# 주어진 surface 버튼을 생성
+	def _drawButton(self, surface):
 		textRect = surface.get_rect()
 		textRect.center = self._buttonRect.center
-		
-		# 버튼 그리기
-		pygame.draw.rect(screen, (0, 128, 0), self._buttonRect)
+
+		pygame.draw.rect(self._screen, (0, 128, 0), self._buttonRect)
 		self._screen.blit(surface, textRect)
+
 		pygame.display.update(self._buttonRect)
+
+	# x, y 좌표가 버튼에 들어왔는지 확인
+	def _isInsideButton(self, mousePos):
+		x = mousePos[0]
+		y = mousePos[1]
+
+		if x < self._buttonRect.left or self._buttonRect.right < x:
+			return False
+		if y < self._buttonRect.top or self._buttonRect.bottom < y:
+			return False
+
+		return True
--- game/main_menu.py
+++ game/main_menu.py
@@ -38,40 +38,61 @@ class MainMenu:
     # 실질적인 메뉴 진행. 여기서 게임 한판 종료까지 쭉 진행한다
     def _run(self):
         while True:
+            mousePos = pygame.mouse.get_pos()
+            mousePressMove = pygame.mouse.get_pressed()
+            mouseLeftPressMove = mousePressMove[0]  # 마우스 누르고 움직일 때
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type == pygame.MOUSEBUTTONUP:
-                    return
 
+                if event.type == pygame.MOUSEBUTTONUP:
+                    self._handleButtonHovering(mousePos)
+                elif event.type == pygame.MOUSEBUTTONDOWN or mouseLeftPressMove:
+                    self._handleButtonPressing(mousePos)
+                else:
+                    self._handleButtonHovering(mousePos)
+
+            # 타이틀은 계속 깜빡 깜빡
             self._changeTitleColor()
 
     def _createTitle(self, x, y):
         self.titleBox = TextBox(self._screen, x, y)
-        self.lastTitleIdx = 0
-        self.lastTickTime = pygame.time.get_ticks()
+        self._lastTitleColorIdx = 0
+        self._lastTitleColorTick = pygame.time.get_ticks()
         self._printTitle()
 
     # 제목을 반짝 반짝 꾸며줌
     def _changeTitleColor(self):
         currentTickTime = pygame.time.get_ticks()
-        elapsedSec = (currentTickTime - self.lastTickTime) / 1000
+        elapsedSec = (currentTickTime - self._lastTitleColorTick) / 1000
         if elapsedSec < self._TitleChangeSec:
             return
 
-        self.lastTitleIdx += 1
-        if self.lastTitleIdx == self._TitleColors.__len__():
-            self.lastTitleIdx = 0
+        self._lastTitleColorIdx += 1
+        if self._lastTitleColorIdx == self._TitleColors.__len__():
+            self._lastTitleColorIdx = 0
         self._printTitle()
 
-        self.lastTickTime = pygame.time.get_ticks()
+        self._lastTitleColorTick = currentTickTime
 
     def _printTitle(self):
-        color = self._TitleColors[self.lastTitleIdx]
+        color = self._TitleColors[self._lastTitleColorIdx]
         self.titleBox.Print("예찬쌤의 리듬 게임", 100, color)
 
     # 버튼 추가
     def _createButton(self, x, y, text):
         button = Button(self._screen, x, y, text)
         self._buttons.append(button)
+
+    # 마우스를 버튼 위에 올렸을 때 처리
+    def _handleButtonHovering(self, mousePos):
+        for button in self._buttons:
+            button.Hovering(mousePos)
+
+    # 마우스를 누른채 버튼 위에 올렸을 때 처리
+    def _handleButtonPressing(self, mousePos):
+        for button in self._buttons:
+            button.Pressing(mousePos)
+    

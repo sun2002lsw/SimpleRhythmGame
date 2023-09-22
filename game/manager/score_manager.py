4b6824dc406503344d1cf454e5a021afc23a0996
sun2002lsw <44559110+sun2002lsw@users.noreply.github.com>
2023-09-22 23:35:29 +0900

    edit combo effect

--- game/manager/score_manager.py
+++ game/manager/score_manager.py
@@ -10,11 +10,13 @@ class ScoreManager:
 
         comboX = width / 2
         comboY = height / 3
-        self._comboTextBox = TextBox(self._screen, comboX, comboY)
+        self._comboStrBox = TextBox(self._screen, comboX, comboY - 70)
+        self._comboDrawBox = TextBox(self._screen, comboX, comboY)
 
         scoreX = width * 5 / 6
         scoreY = height / 2
-        self._scoreTextBox = TextBox(self._screen, scoreX, scoreY)
+        self._scoreStrBox = TextBox(self._screen, scoreX, scoreY - 70)
+        self._scoreDrawBox = TextBox(self._screen, scoreX, scoreY)
 
         self._combo = 0
         self._comboTick = 0
@@ -34,14 +36,15 @@ class ScoreManager:
 
     def _DrawCombo(self):
         effectSec = (pygame.time.get_ticks() - self._comboTick) / 1000
-        strSize = int(max(80 - effectSec * 100, 50))
+        strSize = int(max(100 - effectSec * 100, 50))
+        alpha = int(max(255 - effectSec * 100, 50))
 
-        alpha = int(max(255 - effectSec * 500, 100))
-
-        self._comboTextBox.Print(self._combo, strSize, True, "white", alpha)
+        self._comboStrBox.Print("COMBO", 50, True, "white", alpha)
+        self._comboDrawBox.Print(str(self._combo), strSize, True, "white", alpha)
 
     def _DrawScore(self):
-        self._scoreTextBox.Print(self._score, 50, True, "white", 255)
+        self._scoreStrBox.Print("SCORE", 50, True, "white", 255)
+        self._scoreDrawBox.Print(str(self._score), 50, True, "white", 255)
 
 
 class ComboState(Enum):

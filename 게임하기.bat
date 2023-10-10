9ea3e4d26dcacf695e578616e094216090d124fb
sun2002lsw <limit_speed@naver.com>
2023-10-10 16:09:04 +0900

    update setting file

--- 게임하기.bat
+++ 게임하기.bat
@@ -1,6 +1,12 @@
 @echo off
 
+:: 그냥 마음 편하게 업데이트하고 시작
+pip install --upgrade pip
+pip install pygame
+echo ====================
+
+:: 폴더 이동해서 게임 실행
 cd %~dp0
-cd /d "리듬게임"
+cd .\리듬게임\
 
 python main.py

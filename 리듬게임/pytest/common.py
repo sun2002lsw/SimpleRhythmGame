3c62ca1f17a9b3d2dc73b962afb6ddbfd5ea43b1
sungwook <44559110+sun2002lsw@users.noreply.github.com>
2023-10-18 21:20:20 +0900

    implement pytest (#17)

---
+++ 리듬게임/pytest/common.py
@@ -0,0 +1,47 @@
+import os
+import json
+
+
+def getAbsPath(relativePath):
+    return os.path.join(os.getcwd(), "../" + relativePath)
+
+
+def getRelativePath(absPath):
+    return absPath.split("..")[-1]
+
+
+def getConfig():
+    configPath = getAbsPath("config.json")
+
+    with open(configPath) as file:
+        return json.load(file)
+
+
+def getInvalidJsonFilePath(path):
+    if os.path.isdir(path):
+        return checkDirJsonFiles(path)
+    else:
+        return checkOneJsonFile(path)
+
+
+def checkOneJsonFile(path):
+    with open(path, encoding='UTF8') as file:
+        try:
+            json.load(file)
+        except ValueError as e:
+            return getRelativePath(path)
+
+    return ""
+
+
+def checkDirJsonFiles(path):
+    for jsonFile in os.listdir(path):
+        jsonFilePath = os.path.join(path, jsonFile)
+
+        with open(jsonFilePath, encoding='UTF8') as file:
+            try:
+                json.load(file)
+            except ValueError as e:
+                return getRelativePath(jsonFilePath)
+
+    return ""

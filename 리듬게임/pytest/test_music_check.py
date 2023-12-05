38ef7ebe61d4ea45dd9f7c65f208a30eda5aec01
sungwook Lee <44559110+sun2002lsw@users.noreply.github.com>
2023-12-05 23:09:58 +0900

    add pitch sound file checker (#34)

--- 리듬게임/pytest/test_music_check.py
+++ 리듬게임/pytest/test_music_check.py
@@ -33,24 +33,48 @@ def test_instrument_validation():
         with open(instrumentJsonPath, encoding='UTF8') as file:
             pitchLaneData = json.load(file)
 
-        pitchByLaneTuple = {}
-        for pitch, lanes in pitchLaneData.items():
-            lanes.sort()
-            for idx, lane in enumerate(lanes):
-                if idx > 0:
-                    failMsg = ("duplicate lane detected. (instrument: {0}), (pitch: {1}), (lane: {2})"
-                               .format(instrumentName, pitch, lanes[idx]))
-                    assert lanes[idx - 1] != lanes[idx], failMsg
+            pitchByLaneTuple = {}
+            for pitch, lanes in pitchLaneData.items():
+                lanes.sort()
+                for idx, lane in enumerate(lanes):
+                    if idx > 0:
+                        failMsg = ("duplicate lane detected. (instrument: {0}), (pitch: {1}), (lane: {2})"
+                                   .format(instrumentName, pitch, lanes[idx]))
+                        assert lanes[idx - 1] != lanes[idx], failMsg
 
-            laneTuple = tuple(lanes)
-            failMsg = ("{0} instrument: {1} pitch and {2} pitch have same lanes"
-                       .format(instrumentName, pitchByLaneTuple.get(laneTuple), pitch))
-            assert laneTuple not in pitchByLaneTuple, failMsg
+                laneTuple = tuple(lanes)
+                failMsg = ("{0} instrument - {1} pitch and {2} pitch have same lanes"
+                           .format(instrumentName, pitchByLaneTuple.get(laneTuple), pitch))
+                assert laneTuple not in pitchByLaneTuple, failMsg
 
-            pitchByLaneTuple[laneTuple] = pitch
+                pitchByLaneTuple[laneTuple] = pitch
 
 
 @pytest.mark.order(5)
 # 악기 파일에 정의된 계이름은 sound 악기 폴더에 소리 파일이 있어야 함
 def test_instrument_sound():
-    pass
+    config = getConfig()
+    instrumentPath = getAbsPath(config["musicInstrumentPath"])
+    instrumentSoundPath = getAbsPath(config["musicInstrumentSoundPath"])
+
+    # 특정 악기에 대하여
+    for instrumentJson in os.listdir(instrumentPath):
+        instrumentName = Path(instrumentJson).stem
+        instrumentJsonPath = os.path.join(instrumentPath, instrumentJson)
+
+        # 계이름 목록을 가져오고
+        with open(instrumentJsonPath, encoding='UTF8') as file:
+            pitchLaneData = json.load(file)
+            pitchList = pitchLaneData.keys()
+
+        # 해당 악기의 음악 폴더에서 음악 파일 목록도 가져와서
+        instrumentSoundFilePath = os.path.join(instrumentSoundPath, instrumentName)
+        fileList = os.listdir(instrumentSoundFilePath)
+        fileNameList = [Path(file).stem for file in fileList]
+        fileNameDict = {fileName: 0 for fileName in fileNameList}
+
+        # 계이름과 같은 이름의 파일이 있는지 확인
+        for pitch in pitchList:
+            failMsg = ("{0} instrument - {1} pitch sound file not exist"
+                       .format(instrumentName, pitch))
+            assert pitch in fileNameDict, failMsg
